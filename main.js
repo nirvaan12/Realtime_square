fdc73f8c0912f3fd0a24fe79804b8fc99a338fef
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-03-31 19:44:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -17,9 +22,22 @@ poseNet.on('pose',gotPoses);
     function gotPoses(results){
      if(results.length>0){
          console.log(results);
+         noseX= results[0].pose.nose.x;
+         noseY= results[0].pose.nose.y;
+         console.log("noseX= "+noseX+"and noseY= "+noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX= "+leftWristX+"and rightWristX= "+rightWristX+"and difference= "+difference);
+
      }  
       }
 
       function draw(){
-          background("#272de6")
+          background("#272de6");
+         document.getElementById("square").innerHTML="Width and Height of the square is = "+difference+" px";
+         fill("#03fca1");
+         stroke("#fc4103");
+         square(noseX,noseY,difference);
       }
